cac4c4f14990f95fcdb49393b5249c138cb5032f
xemmel <lacour@gmail.com>
2016-09-26 17:27:47 +0200

    Code set up

---
+++ ThoftPerfion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThoftPerfion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"{ex.Message}\r\n{ex.InnerException?.Message}");
+            }
+        }
+    }
+}
